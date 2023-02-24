3f4cb93c9d536111669e80a613b45232885bf390
Victoria McCrady <spartanginger@gmail.com>
2023-02-23 16:03:26 -0800

    User code & tests started. halfway done with standalone tests

--- src/User.js
+++ src/User.js
@@ -1,5 +1,37 @@
 class User {
-  // User code here
+  //password is protected
+  #password;
+  constructor(username,password,age){
+    if(!username){
+      throw new Error("you must have a username!")
+    } else if(!password){
+      throw new Error("you must have a password!")
+    }else if(!age || age < 1){
+      throw new Error("you must have an age!")
+    }
+    this.username = username;
+    this.#password = password;
+    this.age = age;
+    this.loggedIn = false;
+  }
+
+  login(password){
+    if(this.#password === password){
+      this.loggedIn = true
+    } else {
+      throw new Error("Password is incorrect")
+    }
+  }
+  logout() {
+    this.loggedIn = false;
+  }
+  getPassword(){
+    return this.#password;
+  }
+  setNewPassword(password){
+    this.#password = password;
+    return "Password Changed!"
+  }
 }
 
 module.exports = User
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,55 @@
 const User = require('../src/User')
+describe('is jest working?',() => {
+    test("1 = 1",()=>{
+        expect(1).toBe(1)
+    })
+})
+describe('User object & props',()=>{
+    //base obj for tests
+    const user = new User('foo','bar',25)
+    test('is object',() => {
+        expect(typeof user).toBe("object")
+    })
+    // test sucessful init
+    test('good init',() => {
+        expect(user.username).toBe("foo")
+        expect(user.getPassword()).toBe("bar")
+        expect(user.age).toBe(25)
+    })
+    //error tests
+    describe('init & init errors',()=>{
+        test('No Username',()=>{
+            expect(() => new User("","bar",0)).toThrowError("you must have a username!")
+            expect(() => new User(0)).toThrowError("you must have a username!")
+        })
+        test('No Password',()=>{
+            expect(() => new User("foo","",0)).toThrowError("you must have a password!")
+            expect(() => new User("bar",0)).toThrowError("you must have a password!")
+        })
+        test('No age',() => {
+            expect(() => new User("foo","bar",0)).toThrowError("you must have an age!")
+            expect(() => new User("foo","bar")).toThrowError("you must have an age!")
+        })
+        
+    })
 
-// User tests here
+})
 
-// test username
+describe('User methods',() => {
+    const user = new User("foo","bar",25)
+   // test login error
+   test('login',() => {
+        expect(user.loggedIn).toBe(false)
+    })
+    // test login
+    test('login',() => {
+        user.login(user.getPassword())
+        expect(user.loggedIn).toBe(true)
+    })
+    
+})
 
-// test password
 
-// test age
-
-// test login
 
 // test logout
+// test newpassword
