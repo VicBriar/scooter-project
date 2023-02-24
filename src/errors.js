cbcdf4e43993667c48338fd1309872330239de72
Victoria McCrady <spartanginger@gmail.com>
2023-02-24 13:06:04 -0800

    added error file so that if i want to change an error message, I only have to do it once.

---
+++ src/errors.js
@@ -0,0 +1,14 @@
+
+const errorsObj = {
+needUsr: "you must have a username",
+needPswd: "you must have a password",
+wrongPswd: "password is incorrect",
+exsistUsr: "user already exists, please login instead",
+dsntExstUsr: "user doesn't exsist",
+notNewPsd: "you must enter a new password",
+tooYoung: "you must be at least 18 years old to sign up",
+dummyUserName: "BobbyScooterLover",
+dummyPassWord: "omgIloveSc00tersSoMuch"
+}
+
+module.exports = errorsObj;
