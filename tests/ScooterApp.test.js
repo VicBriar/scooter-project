73f10d9766995f85717c815fa9052090be06e675
Victoria McCrady <spartanginger@gmail.com>
2023-02-23 16:03:57 -0800

    initializing files to my preferences, no code yet.

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -1,7 +1,11 @@
 const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 const ScooterApp = require('../src/ScooterApp')
-
+describe('is jest working?',() => {
+    test("1 = 1",()=>{
+        expect(1).toBe(1)
+    })
+})
 // ScooterApp tests here
 
 // register user
