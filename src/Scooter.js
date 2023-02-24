74e74e0d4f6082af9798a752c1e1c1be64fe4fdf
Victoria McCrady <spartanginger@gmail.com>
2023-02-23 16:02:43 -0800

    scooter code & tests started. Incomplete until I create other classes.

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,35 @@
 class Scooter{
-  // scooter code here
+  static nextSerial = 1;
+  constructor(station){
+    if(!station){
+      throw new Error("Station name required")
+    }
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++
+    this.charge = 100;
+    this.isBroken = false;
+  }
+  //methods
+  rent(user){
+    if(this.isBroken){
+      throw new Error("scooter is broken and cannot be checked out, please try again in 5 seconds :)")
+    } else if(this.charge <= 20) {
+      throw new Error("scooter needs to charge first. Please try again later")
+    }
+    this.station = null;
+    this.user = user;
+  }
+
+  dock(station) {
+    this.station = station
+    this.user = null;
+  }
+  //recharge()
+  //requestRepair()
 }
+
+
 
 
 module.exports = Scooter
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -2,21 +2,82 @@ const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
+describe('scooter object & props', () => {
+  test('scooter init with station error',()=>{
+    expect(()=> scooter = new Scooter()).toThrowError("Station name required")
+  })
+  test('serial number init',()=>{
+    let scooter = new Scooter("South")
+    let scooter1 = new Scooter("South")
+    expect(scooter1.serial).toBe(2);
+  })
+  test('typeof scooter', () => {
+    let scooter = new Scooter("South")
+    expect(typeof scooter).toEqual("object")
+  })
+  test('scooter Station init', ()=> {
+    let scooter = new Scooter("South")
+    expect(scooter.station).toBe("South")
+  })
+  test('user init', ()=>{
+    let scooter = new Scooter("South")
+    expect(scooter.user).toBe(null)
+  })
+  test('charge init',()=>{
+    let scooter = new Scooter("South")
+    expect(scooter.charge).toBe(100)
+  })
+  test('isBroken init',()=>{
+    let scooter = new Scooter("South")
+    expect(scooter.isBroken).toBe(false)
+  })
+  
 })
 
 //Method tests
 describe('scooter methods', () => {
-  // tests here!
-
   //rent method
-
+  describe('rent method', () => {
+  ////broken scooter    
+  test('rent broken scooter error',()=> {
+    let scooter = new Scooter("South")
+    scooter.isBroken = true;
+    expect(() => scooter.rent("bob")).toThrowError("scooter is broken and cannot be checked out, please try again in 5 seconds :)")
+  })
+  ////low battery
+  describe ('rent battery low',() => {
+    //////20
+    test('rent dying scooter error',()=> {
+      let scooter = new Scooter("South")
+      scooter.charge = 20;
+      expect(()=> scooter.rent("bob")).toThrowError("scooter needs to charge first. Please try again later")
+      expect(scooter.user).toBe(null)
+  })
+  ////////15
+  test('rent dying scooter error',()=> {
+    let scooter = new Scooter("South")
+    scooter.charge = 15;
+    expect(()=> scooter.rent("bob")).toThrowError("scooter needs to charge first. Please try again later")
+    expect(scooter.user).toBe(null)
+    })
+  })
+  //is user obj? (CREATE TEST AFTER USER OBJ IS COMPLETE)
+  //user added (string; not obj. see above comment)
+  test('did user get added & is station gone?',()=>{
+    let scooter = new Scooter("South")
+    scooter.rent("bob")
+    expect(scooter.user).toBe("bob")
+    expect(scooter.station).toBe(null)
+  })
+})
   //dock method
+  test('docked to station',()=>{
+    let scooter = new Scooter("South")
+    scooter.rent("bob")
+    scooter.dock("North")
+    expect(scooter.station).toBe("North")
+    expect(scooter.user).toBe(null)
+  })
 
   //requestRepair method
 
